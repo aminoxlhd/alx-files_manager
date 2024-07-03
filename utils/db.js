0a8720bd1d74188ccc76386d04d73991b395891d
aminoxlhd <aminoxlhd@gmail.com>
2024-07-03 16:45:26 +0000

    edit Task 1

--- utils/db.js
+++ utils/db.js
@@ -1,44 +1,40 @@
-const { MongoClient } = require('mongodb');
+import { MongoClient } from 'mongodb';
 
 class DBClient {
-  constructor() {
+  constructor () {
     const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
     const database = process.env.DB_DATABASE || 'files_manager';
-    const uri = `mongodb://${host}:${port}/${database}`;
 
+    const uri = `mongodb://${host}:${port}/${database}`;
     this.client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-    this.client.once('connect', () => console.log('Connected to MongoDB'));
-    this.client.once('error', (error) => console.error('Error connecting to MongoDB:', error));
+    this.client.connect((err) => {
+      if (err) {
+        console.error(`MongoDB connection error: ${err}`);
+      } else {
+        console.log('MongoDB connected');
+      }
+    });
   }
 
-  isAlive() {
+  isAlive () {
     return this.client.isConnected();
   }
 
-  async nbUsers() {
-    try {
-      const db = this.client.db();
-      const count = await db.collection('users').countDocuments();
-      return count;
-    } catch (error) {
-      console.error('Error counting users:', error);
-      return 0;
-    }
+  async nbUsers () {
+    const db = this.client.db();
+    const count = await db.collection('users').countDocuments();
+    return count;
   }
 
-  async nbFiles() {
-    try {
-      const db = this.client.db();
-      const count = await db.collection('files').countDocuments();
-      return count;
-    } catch (error) {
-      console.error('Error counting files:', error);
-      return 0;
-    }
+  async nbFiles () {
+    const db = this.client.db();
+    const count = await db.collection('files').countDocuments();
+    return count;
   }
 }
 
 const dbClient = new DBClient();
-module.exports = dbClient;
+export default dbClient;
+
