f77e377a81b4e49185fef67538b4c12978d53373
aminoxlhd <aminoxlhd@gmail.com>
2024-07-04 02:57:46 +0100

    add Task 6

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,17 +1,12 @@
-const { v4: uuidv4 } = require('uuid');
-const { fs, createWriteStream, mkdirSync, existsSync } = require('fs');
-const { promisify } = require('util');
+const { ObjectId } = require('mongodb');
+
 const {
   getUserByToken,
-  createFile,
   findFileById,
-  findParentFile
+  getFilesByUserAndParentId
 } = require('../utils');
-const { FOLDER_PATH = '/tmp/files_manager' } = process.env;
 
-const writeFileAsync = promisify(fs.writeFile);
-
-exports.postUpload = async (req, res) => {
+exports.getShow = async (req, res) => {
   const token = req.headers['x-token'];
   if (!token) {
     return res.status(401).json({ error: 'Unauthorized' });
@@ -22,56 +17,34 @@ exports.postUpload = async (req, res) => {
     return res.status(401).json({ error: 'Unauthorized' });
   }
 
-  const { name, type, data, parentId, isPublic = false } = req.body;
-
-  if (!name) {
-    return res.status(400).json({ error: 'Missing name' });
+  const fileId = req.params.id;
+  if (!ObjectId.isValid(fileId)) {
+    return res.status(400).json({ error: 'Invalid file ID' });
   }
 
-  const acceptedTypes = ['folder', 'file', 'image'];
-  if (!type || !acceptedTypes.includes(type)) {
-    return res.status(400).json({ error: 'Missing type' });
+  const file = await findFileById(fileId);
+  if (!file || file.userId.toString() !== user._id.toString()) {
+    return res.status(404).json({ error: 'Not found' });
   }
 
-  if (type !== 'folder' && !data) {
-    return res.status(400).json({ error: 'Missing data' });
+  res.status(200).json(file);
+};
+
+exports.getIndex = async (req, res) => {
+  const token = req.headers['x-token'];
+  if (!token) {
+    return res.status(401).json({ error: 'Unauthorized' });
   }
 
-  if (parentId) {
-    const parentFile = await findFileById(parentId);
-    if (!parentFile) {
-      return res.status(400).json({ error: 'Parent not found' });
-    }
-    if (parentFile.type !== 'folder') {
-      return res.status(400).json({ error: 'Parent is not a folder' });
-    }
+  const user = await getUserByToken(token);
+  if (!user) {
+    return res.status(401).json({ error: 'Unauthorized' });
   }
 
-  const newFile = {
-    userId: user._id,
-    name,
-    type,
-    isPublic,
-    parentId: parentId || 0,
-  };
+  const parentId = req.query.parentId || 0;
+  const page = parseInt(req.query.page || 0, 10);
 
-  if (type === 'folder') {
-    await createFile(newFile);
-    return res.status(201).json(newFile);
-  }
+  const files = await getFilesByUserAndParentId(user._id, parentId, page);
 
-  const filename = `${uuidv4()}`;
-  const filePath = `${FOLDER_PATH}/${filename}`;
-
-  if (!existsSync(FOLDER_PATH)) {
-    mkdirSync(FOLDER_PATH, { recursive: true });
-  }
-
-  const writeStream = createWriteStream(filePath);
-  await writeFileAsync(writeStream, Buffer.from(data, 'base64'));
-
-  newFile.localPath = filePath;
-  await createFile(newFile);
-
-  return res.status(201).json(newFile);
+  res.status(200).json(files);
 };
