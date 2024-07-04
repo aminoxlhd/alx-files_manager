ede5055f6a960cf11d432e264d6951f5982d1725
aminoxlhd <aminoxlhd@gmail.com>
2024-07-04 02:59:15 +0100

    add Task 7

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -3,10 +3,10 @@ const { ObjectId } = require('mongodb');
 const {
   getUserByToken,
   findFileById,
-  getFilesByUserAndParentId
+  updateFile
 } = require('../utils');
 
-exports.getShow = async (req, res) => {
+exports.putPublish = async (req, res) => {
   const token = req.headers['x-token'];
   if (!token) {
     return res.status(401).json({ error: 'Unauthorized' });
@@ -27,10 +27,17 @@ exports.getShow = async (req, res) => {
     return res.status(404).json({ error: 'Not found' });
   }
 
-  res.status(200).json(file);
+  const updatedFile = {
+    ...file,
+    isPublic: true,
+  };
+
+  await updateFile(updatedFile);
+
+  res.status(200).json(updatedFile);
 };
 
-exports.getIndex = async (req, res) => {
+exports.putUnpublish = async (req, res) => {
   const token = req.headers['x-token'];
   if (!token) {
     return res.status(401).json({ error: 'Unauthorized' });
@@ -41,10 +48,22 @@ exports.getIndex = async (req, res) => {
     return res.status(401).json({ error: 'Unauthorized' });
   }
 
-  const parentId = req.query.parentId || 0;
-  const page = parseInt(req.query.page || 0, 10);
+  const fileId = req.params.id;
+  if (!ObjectId.isValid(fileId)) {
+    return res.status(400).json({ error: 'Invalid file ID' });
+  }
 
-  const files = await getFilesByUserAndParentId(user._id, parentId, page);
+  const file = await findFileById(fileId);
+  if (!file || file.userId.toString() !== user._id.toString()) {
+    return res.status(404).json({ error: 'Not found' });
+  }
 
-  res.status(200).json(files);
+  const updatedFile = {
+    ...file,
+    isPublic: false,
+  };
+
+  await updateFile(updatedFile);
+
+  res.status(200).json(updatedFile);
 };
